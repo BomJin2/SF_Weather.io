c06491b2641aaec2b4b389c982683e34b66f3b40
Bomjin2 <opjkl4006@naver.com>
2024-11-14 23:43:29 +0900

    초기 state값 Json 파일로 관리

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,111 +1,25 @@
 import { TodayCard, Header, Hourly, KakaoMap, TodayHighlight, SevenDays } from "@/components";
-import { ForecastDay, ForecastTideDay, Weather } from "@/types";
+import { ForecastDay, ForecastTideDay } from "@/types";
 import axios from "axios";
 import { useEffect, useState } from "react";
+import defaultTideData from "@/json/defaultTideData.json";
+import defaultWeatherData from "@/json/defaultWeatherData.json";
 
-const defaultWeatherData: Weather = {
-  current: {
-    cloud: 0,
-    condition: { text: "", icon: "", code: 0 },
-    dewpoint_c: 0,
-    dewpoint_f: 0,
-    feelslike_c: 0,
-    feelslike_f: 0,
-    gust_kph: 0,
-    gust_mph: 0,
-    heatindex_c: 0,
-    heatindex_f: 0,
-    humidity: 0,
-    is_day: 1,
-    last_updated: "",
-    last_updated_epoch: 0,
-    precip_in: 0,
-    precip_mm: 0,
-    pressure_in: 0,
-    pressure_mb: 0,
-    temp_c: 0,
-    temp_f: 0,
-    uv: 0,
-    vis_km: 0,
-    vis_miles: 0,
-    wind_degree: 0,
-    wind_dir: "",
-    wind_kph: 0,
-    wind_mph: 0,
-    windchill_c: 0,
-    windchill_f: 0,
-  },
-  location: {
-    country: "",
-    lat: 0,
-    localtime: "",
-    localtime_epoch: 0,
-    lon: 0,
-    name: "",
-    region: "",
-    tz_id: "",
-  },
-  forecast: { forecastday: [] },
-};
-
-const defaultTideData: ForecastTideDay = {
-  astro: {
-    is_moon_up: 0,
-    is_sun_up: 0,
-    moon_illumination: 0,
-    moon_phase: "",
-    moonrise: "",
-    moonset: "",
-    sunrise: "",
-    sunset: "",
-  },
-  date: "",
-  date_epoch: 0,
-  day: {
-    avghumidity: 0,
-    avgtemp_c: 0,
-    avgtemp_f: 0,
-    avgvis_km: 0,
-    avgvis_miles: 0,
-    condition: { text: "", icon: "", code: 0 },
-    daily_chance_of_rain: 0,
-    daily_chance_of_snow: 0,
-    daily_will_it_rain: 0,
-    daily_will_it_snow: 0,
-    maxtemp_c: 0,
-    maxtemp_f: 0,
-    maxwind_kph: 0,
-    maxwind_mph: 0,
-    mintemp_c: 0,
-    mintemp_f: 0,
-    totalprecip_in: 0,
-    totalprecip_mm: 0,
-    totalsnow_cm: 0,
-    uv: 0,
-    tides: [
-      {
-        tide: [],
-      },
-    ],
-  },
-  hour: [],
-};
+console.log(defaultTideData);
 
 function HomePage() {
+  const [tideData, setTideData] = useState<ForecastTideDay | any>(defaultTideData);
   const [weatherData, setWeatherData] = useState(defaultWeatherData);
-  const [tideData, setTideData] = useState(defaultTideData);
   const [oneWeek, setOneWeek] = useState([]);
 
   const fetchApi = async () => {
     const API_KEY = "82e890c6837844ea9e704743241411";
     const BASE_URL = "http://api.weatherapi.com/v1";
-    //https://api.weatherapi.com/vi/current.json?q=seoul&key=82e890c6837844ea9e704743241411
-
     try {
       /** Promise 인스턴스 방법을 사용했을 땐, resolve에 해당 */
       /** CRUD C: create -> post() R : read -> get() U : update -> put() D : delete -> delete() */
       const res = await axios.get(`${BASE_URL}/forecast.json?q=seoul&days=7&key=${API_KEY}`);
-      console.log(res);
+      // console.log(res);
 
       if (res.status === 200) {
         setWeatherData(res.data);
@@ -115,7 +29,7 @@ function HomePage() {
       console.error(e);
     } finally {
       /** 비동기 로직이 실행되던 실행되지 않던 무조건 실행되어야만 하는 로직이 작성됨 */
-      console.log("fetchApi 호출은 되었음");
+      // console.log("fetchApi 호출은 되었음");
     }
   };
 
@@ -125,7 +39,7 @@ function HomePage() {
     /** https://api.weatherapi.com/v1/marine.json?q=seoul&days=1&key=aaaaaaaaaaaaaa */
     try {
       const res = await axios.get(`${BASE_URL}/marine.json?q=seoul&days=1&key=${API_KEY}`);
-      console.log(res);
+      // console.log(res);
 
       if (res.status === 200) {
         setTideData(res.data.forecast.forecastday[0]);
@@ -133,7 +47,7 @@ function HomePage() {
     } catch (e) {
       console.log(e);
     } finally {
-      console.log("호출은 되었어용");
+      // console.log("호출은 되었어용");
     }
   };
 
@@ -143,7 +57,7 @@ function HomePage() {
     /** https://api.weatherapi.com/v1/marine.json?q=seoul&days=1&key=aaaaaaaaaaaaaa */
     try {
       const res = await axios.get(`${BASE_URL}/forecast.json?q=seoul&days=7&key=${API_KEY}`);
-      console.log(res);
+      // console.log(res);
 
       if (res.status === 200) {
         const newData = res.data.forecast.forecastday.map((item: ForecastDay) => {
@@ -160,7 +74,7 @@ function HomePage() {
     } catch (e) {
       console.log(e);
     } finally {
-      console.log("호출은 되었어용");
+      // console.log("호출은 되었어용");
     }
   };
 
@@ -169,7 +83,7 @@ function HomePage() {
     fetchTideApi();
     getOneWeekWeather();
   }, []);
-  console.log(tideData);
+  // console.log(tideData);
   return (
     <>
       <div className="page ">
@@ -186,6 +100,9 @@ function HomePage() {
             <div className="w-full flex items-center gap-6">
               <TodayHighlight tideData={tideData} currentData={weatherData} />
               <SevenDays data={oneWeek} />
+            </div>
+            <div>
+              <h1>${JSON.stringify(defaultTideData, null, 2)}</h1>
             </div>
           </div>
         </div>
